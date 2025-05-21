a274726ba47475fb4015ffe020b925b5437ac0a0
singupurapusaicharan <22211a05t2@bvrit.ac.in>
2025-05-21 14:29:30 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This is important for GitHub Pages
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
